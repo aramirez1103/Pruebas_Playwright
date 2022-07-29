0451c48548b873f3c57b3aec741d4e3337ae3070
Aime Ramirez <58045945+aramirez1103@users.noreply.github.com>
2022-07-29 15:51:47 -0300

    TareaAimeVersion1

---
+++ tests/test_aime.spec.js
@@ -0,0 +1,15 @@
+const { test, expect, request } = require("@playwright/test");
+
+test("Tarea1: Busqueda en Google", async ({page})=> 
+{
+    await page.goto("https://www.google.com");
+    const cajonDeBusqueda = page.locator(".gLFyf.gsfi");
+    await cajonDeBusqueda.fill("¿Qué le dijo un fideo a otro?");
+    await page.keyboard.press("Enter",{delay:500});
+    //const primerResultado = page.locator("//div[@class='yuRUbf'] >> nth=0 >> a");  //xpath
+    //await primerResultado.click();
+    const primerResultado1 = page.locator(".yuRUbf").first(); 
+    await primerResultado1.locator("a").click();
+
+}
+);
